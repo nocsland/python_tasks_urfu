113f02380d2b1ec825cec258843162d9864b4482
Vlasov <nocsland@gmail.com>
2023-10-31 08:56:36 +0400

    added arrays

---
+++ 4_Arrays/4_4/numpy_4_4.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+mystery = np.array([12279., -26024., 28745., np.nan, 31244., -2365., -6974.,
+                    -9212., np.nan, -17722., 16132., 25933., np.nan, -16431.,
+                    29810.], dtype=np.float32)
+
+nans_index = np.isnan(mystery)
+n_nan = sum(np.isnan(mystery))
+mystery_new = mystery.copy()
+mystery_new[np.isnan(mystery_new)] = 0
+mystery_int = np.int32(mystery)
+array = np.sort(mystery)
+table = array.reshape((5, 3), order='F')
+col = table[:, 1]
